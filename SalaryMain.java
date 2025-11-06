330f47c28817faedf0ec01d9857b7b46577db54d
Koushik1446 <saikoushiknookala144@gmail.com>
2025-11-06 06:34:40 +0530

    Assignment

---
+++ SalaryMain.java
@@ -0,0 +1,30 @@
+package com.codegnan.oopexamples;
+
+import java.util.Scanner;
+
+public class SalaryMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter Employee ID: ");
+        int empId = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Salary: ");
+        double salary = sc.nextDouble();
+
+        System.out.print("Enter Experience (in years): ");
+        int experience = sc.nextInt();
+
+        // Create employee object using full constructor
+        Salary emp = new Salary(empId, name, salary, experience);
+        emp.displayDetails();
+
+        sc.close();
+    }
+}
+
